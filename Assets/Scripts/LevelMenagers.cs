91a106407b9bbf054260044bbd81d0c2f61b4ed0
TheBigMaster106 <piotroler@gmail.com>
2024-04-13 13:42:18 +0200

    dzialaj prosz

---
+++ Assets/Scripts/LevelMenagers.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class LevelManager : MonoBehaviour
+{
+    //pozycja gracza
+    Transform player;
+
+    //prefab przeciwnika
+    public GameObject basherPrefab;
+
+    //czas mi�dzy respawnem kolejnego bashera
+    public float spawnInterval = 1;
+
+    //czas od ostatniego respawnu
+    float timeSinceSpawn;
+
+    //bezpieczna odleg�o�� spawnu
+    float spawnDistance = 30;
+
+    //ilosc punkt�w
+    int points = 0;
+
+    //licznik punkt�w na ekranie
+    public GameObject pointsCounter;
+
+    //licznik czasu na ekranie
+    public GameObject timeCounter;
+
+    //ekran ko�ca gry
+    public GameObject gameOverScreen;
+
+    //czas do ko�ca poziomu
+    public float levelTime = 60f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //zlinkuj aktualna pozycje gracza do zmiennej transform
+        player = GameObject.FindWithTag("Player").transform;
+
+        //zerujemy licznik
+        timeSinceSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dodaj do czasu od ostatniego spawnu czas od ostatniej klatki (ostatni update())
+        timeSinceSpawn += Time.deltaTime;
+
+        //je�eli d�u�ej ni� jedna sekunda
+        if (timeSinceSpawn > spawnInterval)
+        {
+            //wygeneruj losow� pozycje
+            //Vector3 randomPosition = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+
+            //wygeneruj randomow� pozycj� na kole o promieniu 1
+            Vector2 random = Random.insideUnitCircle.normalized;
+
+            //skonwertuj x,y na x,z i zerow� wysoko��
+            Vector3 randomPosition = new Vector3(random.x, 0, random.y);
+
+            //zwielokrotnij odleg�os� od gracza tak, �eby spawn nast�powa� poza kamer�
+            randomPosition *= spawnDistance;
+
+            //dodaj do niej pozycje gracza tak, aby nowe wsp�rz�dne by�y pozycj� wzgl�dem gracza
+            randomPosition += player.position;
+
+            //sprawdz czy danej miejsce jest wolne
+            if (!Physics.CheckSphere(new Vector3(randomPosition.x, 1, randomPosition.z), 0.5f))
+            {
+                //stworz nowego przeciwnika z istniej�cego prefaba, na pozycji randomPosition z rotacj� domy�ln�
+                Instantiate(basherPrefab, randomPosition, Quaternion.identity);
+
+                //wyzeruj licznik
+                timeSinceSpawn = 0;
+            }
+            //je�li miejsce b�dzie zaj�te to program podejmie kolejn� pr�b� w nast�pnej klatce
+
+        }
+
+        //TODO: opracowa� spos�b na przyspieszanie spawnu w niesko�czono�� wraz z d�ugo�cia trwania etapu
+
+        //dodaj do czasu poziomu czas od ostatniej klatki
+
+        if (levelTime < 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            levelTime -= Time.deltaTime;
+            UpdateUI();
+        }
+
+    }
+    public void AddPoints(int amount)
+    {
+        points += amount;
+    }
+    //funkcja kt�ra odpowiada za aktualizacje interfejsu
+    private void UpdateUI()
+    {
+        pointsCounter.GetComponent<TextMeshProUGUI>().text = "Punkty: " + points.ToString();
+        timeCounter.GetComponent<TextMeshProUGUI>().text = Mathf.Floor(levelTime).ToString()=="-1"?"0": Mathf.Floor(levelTime).ToString();
+    }
+    //ta funkcja uruchamia si� je�li gracz zginie lub je�li czas si� sko�czy
+    public void GameOver()
+    {
+        //wy��cz sterowanie gracza
+        player.GetComponent<PlayerController>().enabled = false;
+        player.transform.Find("MainTurret").GetComponent<WeaponController>().enabled = false;
+
+        //wylacz bashery
+        GameObject[] enemyList = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject basher in enemyList)
+        {
+            basher.GetComponent<BasherController>().enabled = false;
+        }
+
+        //wyswietl poprawnie wynik na ekranie ko�cowym
+        gameOverScreen.transform.Find("FinalScoreText").GetComponent<TextMeshProUGUI>().text = "Wynik koncowy: " + points.ToString();
+
+        //poka� ekran ko�ca gry
+        gameOverScreen.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    //zasi�g broni
+    public float range = 10f;
+
+    //transform gracza
+    Transform player;
+
+    //prefab pocisku
+    public GameObject projectilePrefab;
+
+    //spawn pocisku
+    Transform projectileSpawn;
+
+    //czestotliwosc strzalu (/sek)
+    public float rateOfFire = 1;
+    //czas od ostatniego wystrzalu
+    float timeSinceLastFire = 0;
+
+    //moc wystrza�u (pr�dko�c pocz�tkowa)
+    public float projectileForce = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // pozycja gracza
+        player = GameObject.FindWithTag("Player").transform;
+
+        //znajdz w hierarchii obieku miejsce z ktorego staruje pocisk
+        projectileSpawn = transform.Find("ProjectileSpawn").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform target = TagTargeter("Enemy");
+        if (target != transform)
+        {
+            //Debug.Log("Celuje do: " + target.gameObject.name);
+            transform.LookAt(target.position + Vector3.up);
+
+            //wystrzel pocisk
+            //je�li min�o wi�cej od ostatniego strza�u ni� wskazuje na to pr�dko�� strzelania
+            if (timeSinceLastFire > rateOfFire)
+            {
+                //stworz pocisk
+                GameObject projectile = Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity);
+
+
+                //znajdz rrigidbody dla pocisku
+                Rigidbody projectileRB = projectile.GetComponent<Rigidbody>();
+                //"popchnij" pocisk do przodu
+                //sila dziala w kierunku przodu dzia�a (pojectilespawn.z) * si�a wystrza�u
+                projectileRB.AddForce(projectileSpawn.transform.forward * projectileForce, ForceMode.VelocityChange);
+
+                //je�eli strzelisz to wyzeruj czas 
+                timeSinceLastFire = 0;
+
+                //zniszcz pocisk po 5 sekundach
+                Destroy(projectile, 5);
+            }
+            else
+            {
+                timeSinceLastFire += Time.deltaTime;
+            }
+        }
+
+    }
+    Transform TagTargeter(string tag)
+    {
+        //tablica wszystkich obiekt�w pasuj�cych do taga podanego jako agument
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(tag);
+
+        //szukamy najbli�szego
+        Transform closestTarget = transform;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (GameObject target in targets)
+        {
+            //wektor przesuni�cia wzgl�dem gracza
+            Vector3 difference = target.transform.position - player.position;
+            //odleg�o�� od gracza
+            float distance = difference.magnitude;
+
+            if (distance < closestDistance && distance < range)
+            {
+                closestTarget = target.transform;
+                closestDistance = distance;
+            }
+        }
+        return closestTarget;
+    }
+
+    Transform LegeacyTargeter()
+    {
+        //znajdz wszystkie colidery w promieniu = range i zapisz je do tablicy collidersInRange
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, range);
+
+        //do cel�w testowych 
+        //Debug.Log("Ilo�� collider�w w zasi�gu broni: " +  collidersInRange.Length);
+
+        //szukamy najbli�szego przeciwnika
+
+        Transform target = transform;
+        float targetDistance = Mathf.Infinity;
+
+        foreach (Collider collider in collidersInRange)
+        {
+            //wyci�gnij transforma od tego coldiera
+
+            //najpierw znajdz kapsu�e/model (w�a�ciciela colidera)
+            GameObject model = collider.gameObject;
+
+            if (model.transform.parent != null)
+            {
+                //znajdz rodzica modelu czyli przeciwnika
+                GameObject enemy = model.transform.parent.gameObject;
+
+                //sprawdz czy to co znalaz�e� jest przeciwnikiem
+                if (enemy.CompareTag("Enemy"))
+                {
+                    //je�li to przeciwnik to okre�l wektor przesuni�cia
+                    Vector3 diference = player.position - enemy.transform.position;
+                    //policz d�ugo�� wektora (odleg�o��)
+                    float distance = diference.magnitude;
+                    if (distance < targetDistance)
+                    {
+                        //znaleziono nowy cel bli�ej
+                        target = enemy.transform;
+                        targetDistance = distance;
+                    }
+                }
+            }
+
+
+        }
+
+        //do cel�w testowych
+        //Debug.Log("Celuje do: " + target.gameObject.name);
+
+        return target;
+    }
+}
